15bfa0c3736d0ef17b6149a3433bad98a47f9fff
Ndumiso Buthelezi <ndzucain@gmail.com>
2025-05-14 21:31:46 +0200

    lecture update

--- WebApplication1/Controllers/LecturerDashboardController.cs
+++ WebApplication1/Controllers/LecturerDashboardController.cs
@@ -6,9 +6,34 @@ namespace WebApplication1.Controllers
     [Authorize(Roles = "Lecturer")]
     public class LecturerDashboardController : Controller
     {
+        // Main Dashboard
         public IActionResult Index()
         {
             return View();
+        }
+
+        // Upload Study Content
+        public IActionResult ContentUpload()
+        {
+            return View(); // Views/LecturerDashboard/ContentUpload.cshtml
+        }
+
+        // Create Quizzes & Assignments
+        public IActionResult QuizAssignment()
+        {
+            return View(); // Views/LecturerDashboard/QuizAssignment.cshtml
+        }
+
+        // Set Up Online Classes
+        public IActionResult OnlineClass()
+        {
+            return View(); // Views/LecturerDashboard/OnlineClass.cshtml
+        }
+
+        // Track Student Performance
+        public IActionResult Performance()
+        {
+            return View(); // Views/LecturerDashboard/Performance.cshtml
         }
     }
 }
---
+++ WebApplication1/Views/LecturerDashboard/OnlineClass.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.LecturerDashboard
+{
+    public class OnlineClassModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
